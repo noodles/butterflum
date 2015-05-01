1725f423f3cfcadf93a15106d5a5e637fc1d4501
Rich Hall <rjhall@gmail.com>
2015-05-01 15:39:22 +0400

    Completed Chapter 4

--- both/router/routes.js
+++ both/router/routes.js
@@ -6,6 +6,21 @@ Router.route('/dashboard', {
   name: 'dashboard'
 });
 
+Router.route('/customers', {
+  name: 'customers',
+  layoutTemplate:'appLayout'
+});
+
+Router.route('/archives', {
+  name: 'archives',
+  layoutTemplate:'appLayout'
+});
+
+Router.route('/roles', {
+  name: 'roles',
+  layoutTemplate:'appLayout'
+});
+
 Router.plugin('ensureSignedIn', {
   only: ['dashboard']
 });
